51552c71405b8845191cdf2786ceac049dca9b76
Your Name <you@example.com>
2020-11-27 11:25:54 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router'
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -8,7 +10,14 @@ import { ServersComponent } from './servers/servers.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { UserComponent } from './users/user/user.component';
+import { FormsModule } from '@angular/forms';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'servers', component: ServersComponent },
+
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +29,8 @@ import { UserComponent } from './users/user/user.component';
     UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
